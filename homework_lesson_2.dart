fb3acde30bf4fd2db015c5a119a9655516097ce5
hurualqayeva <79925763+hurualqayeva@users.noreply.github.com>
2023-04-28 23:03:18 +0400

    Add files via upload

---
+++ homework_lesson_2.dart
@@ -0,0 +1,16 @@
+import 'dart:io';
+
+main() {
+  ///Task 1:İstifadəçidən adını və yaşını daxil etməyi tələb edən proqram yaradın.
+  ///100 yaşının neçə il sonra tamam olduğunu bildirən bir mesaj çap edin.
+  String name = stdin.readLineSync().toString();
+  int age = int.parse(stdin.readLineSync().toString());
+  int diff = 100 - age;
+  print('$name  $diff il sonra 100 yaşlı olacaq.');
+
+  // Task 2: İstifadəçidən ən azı 5 sözdən ibarət cümlə daxil etməyi tələb edən proqram yaradın.
+  // Cümləni sözlərini əksinə sıralayan bir mesaj çap edin.
+  // Nümunə: dart programlaşdırma dilidir => dilidir proqramlaşdırma dart
+  String text = stdin.readLineSync().toString();
+  print(text.split(' ').reversed.join(' '));
+}
